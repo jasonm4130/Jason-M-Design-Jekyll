c41711407e32d76c068ff8139dfcbea35fe4881f
jasonm4130 <jasonm4130@gmail.com>
2017-06-30 10:08:21 +1000

    added formspree

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,4 +1,76 @@
 /*------------------------------------------
+ Contact form
+ ------------------------------------------*/
+
+$(document).ready(function () {
+
+        $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "https://formspree.io/jasonm4130@gmail.com",
+                method: "POST",
+                data: {
+                    name: name,
+                    email: email,
+                    message: message
+                },
+                dataType: "json",
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#contactFormResponse').html("<div class='alert alert-success'>");
+                    $('#contactFormResponse > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#contactFormResponse > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#contactFormResponse > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#contactFormResponse').html("<div class='alert alert-danger'>");
+                    $('#contactFormResponse > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#contactFormResponse > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#contactFormResponse > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+$('#name').focus(function() {
+    $('#contactFormResponse').html('');
+});
+
+/*------------------------------------------
  Fun-facts counter
  ------------------------------------------*/
 
