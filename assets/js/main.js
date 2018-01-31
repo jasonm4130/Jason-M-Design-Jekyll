642b8bad53d2ad073ff60fe7d885d2e689b655e9
jasonm4130 <jasonm4130@gmail.com>
2018-01-31 11:36:39 +1000

    Contact Form

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,53 +7,30 @@ $(function () {
 	$("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
-            // additional error messages or events
+			// additional error messages or events
+			// Fail message
+			$('#contactFormResponse').html("<div class='alert alert-danger'>");
+			$('#contactFormResponse > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+				.append("</button>");
+			$('#contactFormResponse > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+			$('#contactFormResponse > .alert-danger').append('</div>');
+			//clear all fields
+			$('#contactForm').trigger("reset");
         },
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "https://formspree.io/jasonm4130@gmail.com",
-                method: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    message: message
-                },
-                dataType: "json",
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#contactFormResponse').html("<div class='alert alert-success'>");
-                    $('#contactFormResponse > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#contactFormResponse > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#contactFormResponse > .alert-success')
-                        .append('</div>');
+            // Success message
+			$('#contactFormResponse').html("<div class='alert alert-success'>");
+			$('#contactFormResponse > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+				.append("</button>");
+			$('#contactFormResponse > .alert-success')
+				.append("<strong>Your message has been sent. </strong>");
+			$('#contactFormResponse > .alert-success')
+				.append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#contactFormResponse').html("<div class='alert alert-danger'>");
-                    $('#contactFormResponse > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#contactFormResponse > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#contactFormResponse > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
+			//clear all fields
+			$('#contactForm').trigger("reset");
         },
         filter: function() {
             return $(this).is(":visible");
